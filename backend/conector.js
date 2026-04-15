8a9e9dc02be55c95e84ca2bf1a18d896afce4b4f
Jinx <jinxynet@duck.com>
2026-04-15 19:33:35 +0200

    refactor: Remove test connection

--- backend/conector.js
+++ backend/conector.js
@@ -6,25 +6,3 @@ const pool = mariadb.createPool({
   password: "1111",
   connectionLimit: 5,
 });
-
-// TODO: Remove this code
-async function asyncFunction() {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const rows = await conn.query("SELECT 1 as val");
-    console.log(rows); //[ {val: 1}, meta: ... ]
-    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
-      1,
-      "mariadb",
-    ]);
-    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) conn.end();
-  }
-}
-asyncFunction().then(() => {
-  pool.end();
-});
