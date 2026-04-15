5e20c938f86cb9a7f24d1cc6296340027790fedd
Jinx <jinxynet@duck.com>
2026-04-15 19:50:57 +0200

    fix: DB connector username

--- backend/conector.js
+++ backend/conector.js
@@ -2,7 +2,7 @@ const mariadb = require("mariadb");
 // This credentials don't matter if leaked, they are only available in a local VM
 const pool = mariadb.createPool({
   host: "localhost",
-  user: "root",
+  user: "edgerunner",
   password: "1111",
   connectionLimit: 5,
 });
