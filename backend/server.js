4d19c577bac8528a472374877fd1a8a2e7271184
Jinx <jinxynet@duck.com>
2026-04-15 20:29:45 +0200

    Eliminar placeholder

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// TODO: Add different endpoints with arguments
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
